7c06424538b6533873d5a7e6bbde9838b53e25ad
“Nagendra206” <nagendrababudammati01@gmail.com>
2023-02-07 22:55:12 +0530

    first commit

---
+++ src/main/java/in/javahome/App.java
@@ -0,0 +1,14 @@
+package in.javahome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Devops project" );
+    }
+}
+
